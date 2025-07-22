a24bb70e4dfa01f9fdf3abbc093d1f3abf921290
ninja <astore.a.fabio@gmail.com>
2025-07-22 15:15:42 -0300

    quadratic solver

--- polynomial.py
+++ polynomial.py
@@ -1,10 +1,23 @@
 def get_factors(n: int) -> list[int]:
+    n = abs(n)
     factors = [n, -n]
     for i in range(1, abs((n // 2) + 1)):
         if n % i == 0:
             factors.append(i)
             factors.append(-i)
     return factors
+
+
+def quadratic_solver(a: int | float, b: int | float, c: int | float) -> set[int | float]:
+    sqrt = ((b ** 2) - (4 * a * c)) ** (1 / 2)
+    result = set()
+    result.add(
+        ((-b) - sqrt) / (2 * a)
+    )
+    result.add(
+        ((-b) + sqrt) / (2 * a)
+    )
+    return result
 
 
 def polynomial_possibly_factors(first: int | float, last: int | float) -> set[int | float]:
@@ -24,7 +37,11 @@ def polynomial_solver(terms: list[int | float]) -> list[int | float]:
         result = synthetic_division(factor, terms)
         if result[-1] == 0:
             factors.append(factor)
-
+            terms = result[:-1:]
+    if len(terms) == 3:
+        quadradict_result = quadratic_solver(terms[0], terms[1], terms[2])
+        for solution in quadradict_result:
+            factors.append(solution)
     return factors
 
 
@@ -37,7 +54,5 @@ def synthetic_division(c: int | float, terms: list[int | float]) -> list[int | f
     return result_row
 
 
-terms = [
-    12, -20, 1, 3
-]
+terms = [int(i) for i in str(input()).split(" ")]
 print(polynomial_solver(terms))
