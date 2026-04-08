db8e204218189f09ffda3a7a24421860b9218e43
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2026-04-07 23:14:07 -0300

    ordered set

--- ordered_set.py
+++ ordered_set.py
@@ -6,11 +6,11 @@ class OrderedSet:
         self._unique_set: set[tuple[Any]] = set()
         self._objects = args
         temp_tuple = ()
-        temp_set: set[Any] = set()
+        temp_values: list[Any] = []
         for value in args:
-            if value in temp_set:
+            if value in temp_values:
                 raise ValueError("Must not have duplicates of values")
-            temp_set.add(value)
+            temp_values.append(value)
             temp_tuple = tuple([*temp_tuple, value])
             self._unique_set.add(temp_tuple)
 
@@ -27,4 +27,27 @@ class OrderedSet:
 
     def __repr__(self) -> str:
         return f"OrderedSet({self._objects})"
-    
+
+
+class OtimizedOrderedSet:
+    def __init__(self, *args: Any) -> None:
+        self._objects = args
+        temp_values: list[Any] = []
+        for value in args:
+            if value in temp_values:
+                raise ValueError("Cannot contain duplicate values")
+            temp_values.append(value)
+
+    def __getitem__(self, key: int) -> Any:
+        return self._objects[key]
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, OtimizedOrderedSet):
+            return self._objects == value._objects
+        return False
+
+    def __len__(self) -> int:
+        return len(self._objects)
+
+    def __repr__(self) -> str:
+        return f"OtimizedOrderedSet({self._objects})"
